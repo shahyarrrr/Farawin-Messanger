fc23993def16eb0ba599a99ffa734155c1806e81
shahyarrrr <maximumpainlevel@gmail.com>
2024-05-03 17:57:58 +0330

    nothing ...

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -13,9 +13,11 @@
 </html>
 
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $confirm_password = $_POST['conf-password'];
 }
+
 ?>
