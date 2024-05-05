d8dce1895ac001a7de864ec690bd7d5882e6f262
shahyarrrr <maximumpainlevel@gmail.com>
2024-05-05 12:55:21 +0330

    error handling for confirm password

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -28,13 +28,26 @@
 
 <?php
 
+$registered = true;
+$errors = array();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $confirm_password = $_POST['conf-password'];
-    echo "username : $username <br />";
-    echo "password : $password <br />";
-    echo "re-enterd password : $confirm_password <br />";
+    if ($password == $confirm_password) {
+        $password = hash('sha256', $password);
+    } else {
+        $errors[] = 'password does not match with re-enter password field';
+    }
+    if (count($errors) > 0) {
+        $registered = false;
+        foreach ($errors as $error) {
+            echo $error;
+        }
+    }
 }
+
+
 
 ?>
